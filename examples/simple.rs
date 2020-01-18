e6c2db43afef296385d32c896b1ecd194b47b2be
Jacob <jacobrosenthal@gmail.com>
2020-01-18 12:07:32 -0700

    caffeinate test

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,16 +1,42 @@
-extern crate sysbar;
+use std::process;
+use std::sync::mpsc::channel;
+use std::{thread, time};
+
+enum Status {
+    Quit,
+}
 
 fn main() {
-    let mut bar = sysbar::Sysbar::new("Foo");
+    let (tx, rx) = channel();
 
-    bar.add_item(
-        "Say 'bar'",
+    thread::spawn(move || {
+        //ctrlc appears to be killing child process fine as well
+        let mut handle = process::Command::new("caffeinate")
+            .arg("-d")
+            .spawn()
+            .expect("failed to execute process");
+
+        loop {
+            thread::sleep(time::Duration::from_millis(1000));
+
+            if rx.recv().is_ok() {
+                handle.kill().unwrap();
+                process::exit(0);
+            }
+        }
+    });
+
+    let mut menubar = sysbar::Sysbar::new("C");
+
+    //built in quit terminates from objc, and doesnt clean up
+    //so build our own
+    menubar.add_item(
+        "Quit",
         Box::new(move || {
-            println!("bar");
+            tx.send(Status::Quit).unwrap();
         }),
     );
 
-    bar.add_quit_item("Quit");
-
-    bar.display();
+    //menubar is blocking and needs to be on the main thread
+    menubar.display();
 }
